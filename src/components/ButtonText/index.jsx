d895f13625c1c23e5ffad2b3c0affb89b00b4ef1
Elaine Leandro <elainerleandro1@gmail.com>
2023-05-05 08:53:09 -0300

    Adção com input, buttonTex, home.

--- src/components/ButtonText/index.jsx
+++ src/components/ButtonText/index.jsx
@@ -1,10 +1,11 @@
 import {Container} from './styles'
 
 // eslint-disable-next-line react/prop-types
-export function ButtonText({title, ...rest }){
+export function ButtonText({title, isActive = false, ...rest }){
   return(
     <Container 
       type="button" 
+      isActive={isActive} 
       {...rest}
     >
       {title}
---
+++ src/components/input/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+
+  background-color: ${({ theme}) => theme.COLORS.BACKGROUND_900};
+  color: ${({theme}) => theme.COLORS.GRAY_300};
+
+  margin-bottom: 8px;
+`;
