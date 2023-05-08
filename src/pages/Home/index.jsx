cf2d3537954f23deb8203c9a2201486d9d2a7b2b
Elaine Leandro <elainerleandro1@gmail.com>
2023-05-08 17:25:38 -0300

    Criação componete Note, e ajuste no layout home.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,9 @@ import {Container, Brand, Menu, Search, Content, NewNote} from './styles'
 
 import {Header} from '../../components/Header'
 import {ButtonText} from '../../components/ButtonText'
-import { Input } from '../../components/input'
+import { Input } from '../../components/Input'
+import { Section } from '../../components/Section'
+import { Note } from '../../components/Note'
 
 export function Home(){
   return(
@@ -26,6 +28,17 @@ export function Home(){
       </Search>
 
       <Content>
+        <Section title="Minhas Notas">
+          <Note data={{
+            title: 'React',
+            tags : [
+              {id: '1' , name: 'react'},
+              {id: '2' , name: 'rocketseat'}
+            ]
+            }}
+          />
+         
+        </Section>
     
       </Content>
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -49,6 +49,8 @@ padding: 64px 64px 0;
 `
 export const Content = styled.div`
   grid-area: content;
+  padding: 0 64px;
+  overflow-y: auto;
 `
 export const NewNote = styled.button`
   grid-area: newnote;
