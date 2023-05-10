43b351344e32e0d9ec0b40540b23796b6383d009
Elaine Leandro <elainerleandro1@gmail.com>
2023-05-10 19:41:32 -0300

    Componente SignIn.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Home } from './pages/Home'
+// import { Home } from './pages/Home'
 import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
+import { SignIn } from './pages/SignIn'
 
 
 
@@ -12,7 +13,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Home />
+      {/* <Home /> */}
+      <SignIn/>
      
     </ThemeProvider>
 
