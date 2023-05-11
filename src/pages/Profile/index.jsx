22d4500baadf118c179cafaf1eca3dfbb91d7217
Elaine Leandro <elainerleandro1@gmail.com>
2023-05-11 14:31:30 -0300

    Ajuste na Profile

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,18 +1,54 @@
-import {FiArrowLeft, FiUser, FiMail, FiLock} from 'react-icons/fi'
-import {Container} from './styles'
+import {FiArrowLeft, FiUser, FiMail, FiLock, FiCamera} from 'react-icons/fi'
+import {Container, Form, Avatar} from './styles'
 
 import { Input } from '../../components/input'
+import { Button } from '../../components/Button'
 
 export function Profile(){
   return(
     <Container>
       <header>
-        <a href="#">
+        <a href="/">
           <FiArrowLeft/>
         </a>
       </header>
       <Form>
+        <Avatar>
+          <img src="https://github.com/ElaineLeandro.png" 
+          alt="Foto do usuário"/>
 
+          <label htmlFor="avatar">
+            <FiCamera/>
+
+            <input 
+              id="avatar" 
+              type="file"
+            />
+          </label>
+        </Avatar>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+        />
+        <Input
+          placeholder="Senha Atual"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Input
+          placeholder="Nova senha"
+          type="password"
+          icon={FiLock}
+        />
+
+        <Button title="Salvar"/>
       </Form>
     </Container>
   )
