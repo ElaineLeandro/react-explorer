7f4e4fc70d15b03144b4a8bd1c754e7e5c06545f
Elaine Leandro <elainerleandro1@gmail.com>
2023-05-17 14:52:56 -0300

    Routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import { Home } from './pages/Home'
 import GlobalStyles from './styles/global'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
-// import { SignIn } from './pages/SignIn'
-// import { SignUp } from './pages/SignUp'
-// import { Profile } from './pages/Profile'
-import { New } from './pages/New'
+import { Routes } from './routes'
 
 
 
@@ -15,11 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode >
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      {/* <Home /> */}
-      {/* <SignIn/> */}
-      {/* <Profile/>  */}
-      <New/>
-     
+      <Routes/>
     </ThemeProvider>
 
   </React.StrictMode>,
